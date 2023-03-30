b4caef260f899dcd27b656f023fdc21323cc8197
Isglassen <57827077+Isglassen@users.noreply.github.com>
2023-03-30 13:16:53 +0200

    Påbörjade headern för navigation

--- src/header.ts
+++ src/header.ts
@@ -5,7 +5,9 @@ type pagesContainer = {
   }
 }
 
-type pages = {
+type pages = page[]
+
+type page = {
   color: string;
   name: string;
   categories: pages;
@@ -15,8 +17,55 @@ type pages = {
 class HeaderControl {
   element: JQuery<HTMLElement>
 
-  constructor(current: string[], contentJson: pagesContainer) {
+  public getTextColor(rgbString: string) {
+    const values = rgbString.slice(rgbString.indexOf('(') + 1, rgbString.indexOf(')')).split(',')
+    const rgb: { r: number, g: number, b: number } = { r: null, g: null, b: null }
+
+    rgb.r = parseInt(values[0])
+    rgb.g = parseInt(values[1])
+    rgb.b = parseInt(values[2])
+
+    const brightness = Math.round(((rgb[0] * 299) +
+      (rgb[1] * 587) +
+      (rgb[2] * 114)) / 1000);
+    const textColour = (brightness > 125) ? 'black' : 'white';
+
+    return textColour
+  }
+
+  constructor(current: string[], contentJson: pagesContainer, currentName: string) {
+    let currentData: page = { color: "", name: "", link: "", categories: contentJson.pages };
+    current.forEach((name) => {
+      currentData = currentData.categories.filter((value) => value.name == name)[0]
+      console.log(currentData);
+    })
+
     let element = $("#header");
     this.element = element;
+
+    console.log(this.element);
+
+    element.css("display", "flex");
+    element.css("background-color", currentData.color);
+
+    element.css("color", this.getTextColor(element.css("background-color")))
+
+    const pageTitle = document.createElement("div");
+    pageTitle.style.display = "flex";
+    pageTitle.style.flexDirection = "column";
+    pageTitle.style.textAlign = "center";
+
+    const pageGroup = document.createElement("span");
+    pageGroup.appendChild(document.createTextNode(currentData.name));
+    pageGroup.id = "main-title";
+
+    const currentCatagory = document.createElement("span");
+    currentCatagory.appendChild(document.createTextNode(currentName));
+    currentCatagory.id = "sub-title";
+
+    pageTitle.appendChild(pageGroup);
+    pageTitle.appendChild(currentCatagory);
+
+    element.append(pageTitle);
   }
 }
