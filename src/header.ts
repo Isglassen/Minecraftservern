1023e299d656b0e37f75005668fecb18c26d4c46
Isglassen <57827077+Isglassen@users.noreply.github.com>
2023-04-20 13:11:31 +0200

    Started work on navigation in header

--- src/header.ts
+++ src/header.ts
@@ -15,7 +15,8 @@ type page = {
 }
 
 class HeaderControl {
-  element: JQuery<HTMLElement>
+  pageData: pagesContainer;
+  element: JQuery<HTMLElement>;
 
   public getTextColor(rgbString: string) {
     const values = rgbString.slice(rgbString.indexOf('(') + 1, rgbString.indexOf(')')).split(',')
@@ -35,14 +36,23 @@ class HeaderControl {
     return textColour
   }
 
-  constructor(current: string[], contentJson: pagesContainer, currentName: string) {
+  constructor(current: string[], contentJson: pagesContainer) {
+    this.pageData = contentJson;
+
     let currentData: page = { color: "", name: "", link: "", categories: contentJson.pages };
+    const links: URL[] = [];
+
+    const basePath = contentJson.options?.basePath != null ? contentJson.options.basePath : window.location.origin;
+
     current.forEach((name) => {
       currentData = currentData.categories.filter((value) => value.name == name)[0]
       console.log(currentData);
-    })
+      links.push(new URL(currentData.link, basePath));
+    });
 
-    let element = $("#header");
+    console.log(links);
+
+    const element = $("#header");
     this.element = element;
 
     console.log(this.element);
@@ -57,17 +67,57 @@ class HeaderControl {
     pageTitle.style.flexDirection = "column";
     pageTitle.style.textAlign = "center";
 
-    const pageGroup = document.createElement("span");
-    pageGroup.appendChild(document.createTextNode(currentData.name));
+    // TODO: Each of these should actually be a link
+    const pageGroup = document.createElement("a");
+    pageGroup.appendChild(document.createTextNode(current[0]));
+    pageGroup.href = links[0].href;
     pageGroup.id = "main-title";
 
     const currentCatagory = document.createElement("span");
-    currentCatagory.appendChild(document.createTextNode(currentName));
     currentCatagory.id = "sub-title";
+
+    const subcategories = [...current];
+    subcategories.shift();
+
+    for (let i = 0; i < subcategories.length; i++) {
+      const link = document.createElement('a');
+      link.classList.add('subcategory');
+      link.href = links[i+1].href;
+      link.appendChild(document.createTextNode(subcategories[i]));
+
+      currentCatagory.appendChild(link);
+      
+      if (i + 1 != subcategories.length) {
+        currentCatagory.appendChild(document.createTextNode(' > '));
+      }
+    }
 
     pageTitle.appendChild(pageGroup);
     pageTitle.appendChild(currentCatagory);
 
+    const menues = document.createElement('div');
+    menues.id = 'categories';
+
+    contentJson.pages.forEach((page) => {
+      const pageInteractable = document.createElement('span');
+      pageInteractable.appendChild(document.createTextNode(page.name));
+      pageInteractable.classList.add('major-category');
+      pageInteractable.addEventListener('click', () => {
+        this.openSelector(page.name);
+      });
+
+      menues.appendChild(pageInteractable);
+    });
+
     element.append(pageTitle);
+    element.append(menues);
+  }
+
+  closeSelector() {
+    // TODO
+  }
+
+  openSelector(id: string) {
+    // TODO
   }
 }
