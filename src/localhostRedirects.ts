14c09c5ffc6aebff715f0a9f75945dc1a44f2c81
Isglassen <vilhelmgg@gmail.com>
2023-04-30 19:05:37 +0200

    Now automatically get header for url

--- src/localhostRedirects.ts
+++ src/localhostRedirects.ts
@@ -5,3 +5,21 @@ if (window.location.hostname == 'localhost') {
     window.location.pathname += '/';
   }
 }
+
+/* exported FixedUrl */
+class FixedURL extends URL {
+  static fixURLobj(url: URL) {
+    if (url.pathname.endsWith('index.html')) {
+      url.pathname = url.pathname.substring(0, url.pathname.length - 'index.html'.length);
+    } else if (!url.pathname.endsWith('/')) {
+      url.pathname += '/';
+    }
+    return url;
+  }
+
+  constructor(url: string | URL, base?: string | URL | undefined) {
+    super(url, base);
+
+    FixedURL.fixURLobj(this);
+  }
+}
